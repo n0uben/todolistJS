934b74c5732e45549f897b0b648569fa397f0f55
n0uben <benjamin.noureux@gmail.com>
2022-03-10 16:45:10 +0100

    Cree class tacheBDD

---
+++ index.js
@@ -0,0 +1,22 @@
+class TacheBDD {
+
+    baseUrl = "http://localhost:9090/api/taches/";
+
+    getAll() {
+        // fetch(baseUrl)
+        // .then()
+        // .catch()
+    }
+    ajouter(tache) {
+
+    }
+    modifier(tache) {
+
+    }
+    terminer(tache) {
+
+    }
+    supprimer(tache) {
+
+    }
+}
