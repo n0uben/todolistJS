4f4190427762652d51e2ddc11c251f6bf3cd5fb8
lucasfarroni <farronilucas@hotmail.fr>
2022-03-10 16:44:24 +0100

    cree la class tache

---
+++ index.js
@@ -0,0 +1,42 @@
+class tache{
+    id;
+    contenu;
+    date;
+    terminee;
+
+    constructor(contenu,terminee){
+
+    }
+
+
+    getid() {
+
+    }
+    getcontenu() {
+
+    }
+    getdate() {
+
+    }
+    getterminee() {
+
+    }
+
+
+    setcontenu(contenu) {
+        this.contenu = contenu;
+
+    }
+    setterminee(terminee) {
+        this.terminee = terminee;
+
+    }
+    toString() {
+        return this.toString();
+
+    }
+    equals(other) {
+//à faire
+    }
+
+}
