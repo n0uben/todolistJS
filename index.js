166554489ad62bf272ff5e92815f6270dcf92f0b
n0uben <benjamin.noureux@gmail.com>
2022-03-23 18:42:42 +0100

    Fait un peu de rangement

--- index.js
+++ index.js
@@ -1,91 +1,20 @@
-    class TacheBDD {
+/////////////////////////////////////////////////////////////
+// APITACHE
+/////////////////////////////////////////////////////////////
+
+class ApiTaches {
     static baseUrl = "http://localhost:9090/api/taches/";
 
     static async getAll() {
-
         let reponse = await fetch(this.baseUrl);
         let json = await reponse.json();
-    
+
         return json;
-
-    }
-
-    static afficherTaches() {
-       
-        //vide la liste des taches
-        let listeTaches = document.getElementById("listeTaches");
-        listeTaches.innerHTML = "";
-        
-        // recupere toutes les taches et les insere dans le HTML
-        this.getAll()
-            .then((json) => {
-                console.log(json);
-                for (const element of json) {
-                    
-                    let nouvelleTache = new Tache(
-                        element.id,
-                        element.date,
-                        element.description,
-                        element.terminee
-                        );
-                    nouvelleTache.afficher();
-                    console.log(nouvelleTache)
-                }
-            })
-            .catch((error) => console.error(error));
-    }
-        
-    static afficherTachesEnCours() {
-        let listeTaches = document.getElementById("listeTaches");
-        listeTaches.innerHTML = "";
-        
-        this.getAll()
-            .then((json) => {
-                for (const element of json) {
-                    if (!element.terminee) {
-                        let nouvelleTache = new Tache(
-                            element.id,
-                            element.date,
-                            element.description,
-                            element.terminee
-                        );
-                        nouvelleTache.afficher();
-                    }
-                }
-            }
-            
-            )
-    }
-    static afficherTachesTerminees() {
-        let listeTaches = document.getElementById("listeTaches");
-        listeTaches.innerHTML = "";
-        
-        this.getAll()
-            .then((json) => {
-                for (const element of json) {
-                    if (element.terminee) {
-                        let nouvelleTache = new Tache(
-                            element.id,
-                            element.date,
-                            element.description,
-                            element.terminee
-                        );
-                        nouvelleTache.afficher();
-                    }
-                }
-            }
-
-            )
-    }
-    static refreshInterface() {
-        //on vide l'input et on rafraichit la liste des taches
-        document.getElementById("inputAjouter").value = "";
-        this.afficherTaches();
     }
 
     static async enregistrer(tache) {
         let descriptionTache = {
-            description: tache.description
+            description: tache.description,
         };
         console.log(descriptionTache);
 
@@ -94,69 +23,61 @@
             body: JSON.stringify(descriptionTache),
             headers: { "Content-type": "application/json; charset=UTF-8" },
         }).catch((err) => console.log(err));
-        console.log("apres la function?");
 
         return tacheEnregistree;
-
     }
     ///////////////////////////////////////////////////////////////////
 
     static modifier(tacheId) {
-
-        let nouvelleDescription=prompt("quelle est la nouvelle Tâche ?");
+        let nouvelleDescription = prompt("quelle est la nouvelle Tâche ?");
         //console.log(nouvelleDescription);
-        
+
         let descriptionTache = {
             description: nouvelleDescription,
         };
-        fetch(this.baseUrl + tacheId , {
+        fetch(this.baseUrl + tacheId, {
             method: "PUT",
             body: JSON.stringify(descriptionTache),
             headers: { "Content-type": "application/json; charset=UTF-8" },
         })
-        .then(function(response){
-            console.log(response);
-            TacheBDD.refreshInterface();
-            //console.log("OUAIIIII");
-        })
+            .then(function (response) {
+                ListeTaches.rafraichir();
+                //console.log("OUAIIIII");
+            })
             .catch((err) => console.log(err));
     }
     /////////////////////////////////////////////////////////////////////
     static terminer(tacheId) {
-        fetch(this.baseUrl + tacheId + "/terminer",{
+        fetch(this.baseUrl + tacheId + "/terminer", {
             method: "PUT",
-            headers: {"Content-type":"application/json; charset=UTF-8"}
-            
+            headers: { "Content-type": "application/json; charset=UTF-8" },
         })
-        .then(function(response){
-            console.log(response);
-            //console.log("tas recup la reponse franchement tes un bg");
-            
-            TacheBDD.refreshInterface();
-        })
-        .catch(err => console.log(err))
+            .then(function (response) {
+                console.log(response);
+                //console.log("tas recup la reponse franchement tes un bg");
+
+                ListeTaches.rafraichir();
+            })
+            .catch((err) => console.log(err));
         //console.log("OOOOUUAAA");
     }
     /////////////////////////////////////////////////////////////////////
     static supprimer(tacheId) {
         console.log();
-        fetch(this.baseUrl + tacheId,{
-                method: "DELETE",
-                
-                headers: {"Content-type":"application/json; charset=UTF-8"}
-            })
-            .then(function(){
-                TacheBDD.refreshInterface();//une fois la promesse reçu alors->refresh interface
-                
+        fetch(this.baseUrl + tacheId, {
+            method: "DELETE",
 
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+        })
+            .then(function () {
+                ListeTaches.rafraichir(); //une fois la promesse reçu alors->refresh interface
             })
-            .catch(err => console.log(err))
-        
-
+            .catch((err) => console.log(err));
     }
-        
 }
-/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+// TACHE
+/////////////////////////////////////////////////////////////
 class Tache {
     id;
     date;
@@ -198,97 +119,177 @@ class Tache {
         //à faire
     }
 
+    static messageAvantSuppression(tacheId) {
+        let confirmationSup = confirm("Voulez vous supprimer cette tâche ?");
 
-static messageAvantSuppression(tacheId) {
-    
-    let confirmationSup = confirm("Voulez vous supprimer cette tâche ?");
-
-    if(confirmationSup){
-        TacheBDD.supprimer(tacheId);
-    }
-}
-
-afficher() {
-    let listeTaches = document.getElementById("listeTaches");
-
-    let tacheCochee = "";
-    let tacheDesactivee = "";
-
-    if (this.getTerminee()) {
-        tacheCochee = "checked";
-        tacheDesactivee = "disabled";
+        if (confirmationSup) {
+            ApiTaches.supprimer(tacheId);
+        }
     }
 
-    
+    // getHTML() {
+    //     let tacheCochee = "";
+    //     let tacheDesactivee = "";
 
-    let htmlTache = 
-    
-    `<div class="les-taches col-12 col-xl-10 offset-xl-1 p-4 mb-3 shadow-sm bg-white">
-        <div class="row">
-            <div class="col-12 mb-3 col-lg-6 d-flex align-items-center form-check">
-                <input class="form-check-input mx-4 p-3 bg-success" type="checkbox" id="checkbox${this.getid()}" ${tacheCochee} ${tacheDesactivee} onChange="TacheBDD.terminer(${this.getid()})">
-                <label class="form-check-label" for="checkbox${this.getid()}">${this.getdescription()}</label>
+    //     if (this.getTerminee()) {
+    //         tacheCochee = "checked";
+    //         tacheDesactivee = "disabled";
+    //     }
+
+    //     let htmlTache = `<div class="les-taches col-12 col-xl-10 offset-xl-1 p-4 mb-3 shadow-sm bg-white">
+    //     <div class="row">
+    //         <div class="col-12 mb-3 col-lg-6 d-flex align-items-center form-check">
+    //             <input class="form-check-input mx-4 p-3 bg-success" type="checkbox" id="checkbox${this.getid()}" ${tacheCochee} ${tacheDesactivee} onChange="ApiTaches.terminer(${this.getid()})">
+    //             <label class="form-check-label" for="checkbox${this.getid()}">${this.getdescription()}</label>
+    //         </div>
+    //         <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+    //             <button type="button" id="supprimer${this.getid()}" onclick="Tache.messageAvantSuppression(${this.getid()})" class="btn btn-outline-danger mx-1">Supprimer</button>
+    //             <button type="button" id="modifier${this.getid()}" onclick="ApiTaches.modifier(${this.getid()})" class="btn btn-outline-primary mx-1">Modifier</button>
+    //         </div>
+    //     </div>
+    // </div>`;
+    //     return htmlTache;
+    // }
+    afficher() {
+        let listeTaches = document.querySelector("#listeTaches");
+
+        let tacheCochee = "";
+        let tacheDesactivee = "";
+
+        if (this.getTerminee()) {
+            tacheCochee = "checked";
+            tacheDesactivee = "disabled";
+        }
+
+        let htmlTache = `<div id="tache${this.getid()}" class="les-taches col-12 col-xl-10 offset-xl-1 p-4 mb-3 shadow-sm bg-white">
+            <div class="row">
+                <div id="input${this.getid()}" class="col-12 mb-3 col-lg-6 d-flex align-items-center form-check">
+                    <input class="form-check-input mx-4 p-3" type="checkbox" id="checkbox${this.getid()}" ${tacheCochee} ${tacheDesactivee} onChange="ApiTaches.terminer(${this.getid()})">
+                    <label class="form-check-label" for="checkbox${this.getid()}">${this.getdescription()}</label>
+                </div>
+                <div class="col-12 col-lg-6 d-flex justify-content-end align-items-center">
+                <button type="button" id="modifier${this.getid()}" onclick="ApiTaches.modifier(${this.getid()})" class="bouton-modifier btn btn-outline-primary mx-1"><i class="fas fa-edit"></i></button>
+                <button type="button" id="supprimer${this.getid()}" onclick="Tache.messageAvantSuppression(${this.getid()})" class="bouton-supprimer btn btn-outline-danger mx-1"><i class="far fa-trash-alt"></i></button>
+                </div>
             </div>
-            <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
-                <button type="button" id="supprimer${this.getid()}" onclick="Tache.messageAvantSuppression(${this.getid()})" class="btn btn-outline-danger mx-1">Supprimer</button>
-                <button type="button" id="modifier${this.getid()}" onclick="TacheBDD.modifier(${this.getid()})" class="btn btn-outline-primary mx-1">Modifier</button>
-            </div>
-        </div>
-    </div>`;
+        </div>`;
 
-    listeTaches.innerHTML += htmlTache;
-}
-}
-
-
-/////////////////////////////////////////////////////////////
-// INTERFACE
-/////////////////////////////////////////////////////////////
-
-// class interfaceTaches {
-
-//     static test;
-
-//     static afficherTout() {
-//         console.log("coucou");
-//     }
-// }
-
-
-/* clic bouton tout */
-const boutonTout = document.getElementById("afficherTout");
-boutonTout.addEventListener("click", () => TacheBDD.afficherTaches());
-
-/* clic bouton en cours */
-const boutonEnCours = document.getElementById("afficherEnCours");
-console.log(boutonEnCours);
-boutonEnCours.addEventListener("click", () => TacheBDD.afficherTachesEnCours());
-
-
-/* clic bouton terminées */
-const boutonAfficherTerminees = document.getElementById("afficherTerminees");
-console.log(boutonAfficherTerminees);
-boutonAfficherTerminees.addEventListener("click", () => TacheBDD.afficherTachesTerminees());
-
-/* clic bouton ajouter une tache */ 
-const boutonAjouter = document.getElementById("button-addon");
-const inputAjouter = document.getElementById("inputAjouter");
-//const boutonSupprimer = document.getElementById("supprimer${this.getid()}");
-
-boutonAjouter.addEventListener("click", () => {
-    const value = inputAjouter.value;
-    const regex = /[a-z0-9]{1,255}/gi;
-    if (value.match(regex) ) {
-        const maTache = new Tache(null, null, inputAjouter.value, null);
-        TacheBDD.enregistrer(maTache).then(() => TacheBDD.refreshInterface());//une fois la promesse reçu alors->refresh interface
+        listeTaches.insertAdjacentHTML("beforeend", htmlTache);
     }
-});
-
+}
 
 /////////////////////////////////////////////////////////////
+// LISTE TACHE
+/////////////////////////////////////////////////////////////
 
-// initialise l’interface avec la liste de toutes les taches
-TacheBDD.afficherTaches();
+class ListeTaches {
+    static boutonTout = document.getElementById("afficherTout");
+    static boutonEnCours = document.getElementById("afficherEnCours");
+    static boutonTerminees = document.getElementById("afficherTerminees");
 
-//const maTache2 = new Tache(1, null, null, null);
-//TacheBDD.supprimer(maTache2);
+    static listeTaches = document.getElementById("listeTaches");
+
+    static vider() {
+        ListeTaches.listeTaches.innerHTML = "";
+    }
+
+    static afficherTaches() {
+        //vide la liste des taches
+        ListeTaches.vider();
+        // recupere toutes les taches et les insere dans le HTML
+        ApiTaches.getAll()
+            .then((json) => {
+                console.log(json);
+                for (const element of json) {
+                    let nouvelleTache = new Tache(
+                        element.id,
+                        element.date,
+                        element.description,
+                        element.terminee
+                    );
+                    nouvelleTache.afficher();
+                }
+            })
+            .catch((error) => console.error(error));
+    }
+
+    static afficherEnCours() {
+        ListeTaches.vider();
+
+        ApiTaches.getAll().then((json) => {
+            for (const element of json) {
+                if (!element.terminee) {
+                    let nouvelleTache = new Tache(
+                        element.id,
+                        element.date,
+                        element.description,
+                        element.terminee
+                    );
+                    nouvelleTache.afficher();
+                }
+            }
+        });
+    }
+    static afficherTerminees() {
+        ListeTaches.vider();
+
+        ApiTaches.getAll().then((json) => {
+            for (const element of json) {
+                if (element.terminee) {
+                    let nouvelleTache = new Tache(
+                        element.id,
+                        element.date,
+                        element.description,
+                        element.terminee
+                    );
+                    nouvelleTache.afficher();
+                }
+            }
+        });
+    }
+    static rafraichir() {
+        //on vide l'input et on rafraichit la liste des taches
+        document.getElementById("inputAjouter").value = "";
+        this.afficherTaches();
+    }
+    static initFiltres() {
+        boutonTout.addEventListener("click", () =>
+            ListeTaches.afficherTaches()
+        );
+
+        boutonEnCours.addEventListener("click", () =>
+            ListeTaches.afficherEnCours()
+        );
+
+        boutonTerminees.addEventListener("click", () =>
+            ListeTaches.afficherTerminees()
+        );
+    }
+    static initUserInput() {
+        /* clic bouton ajouter une tache */
+        const boutonAjouter = document.getElementById("button-addon");
+        const inputAjouter = document.getElementById("inputAjouter");
+        //const boutonSupprimer = document.getElementById("supprimer${this.getid()}");
+
+        boutonAjouter.addEventListener("click", () => {
+            const value = inputAjouter.value;
+            const regex = /[a-z0-9]{1,255}/gi;
+            if (value.match(regex)) {
+                const maTache = new Tache(null, null, inputAjouter.value, null);
+                ApiTaches.enregistrer(maTache).then(() =>
+                    ListeTaches.rafraichir()
+                ); //une fois la promesse reçu alors->refresh interface
+            }
+        });
+    }
+}
+
+/////////////////////////////////////////////////////////////
+// LETSGO
+/////////////////////////////////////////////////////////////
+
+ListeTaches.afficherTaches();
+
+ListeTaches.initFiltres();
+
+ListeTaches.initUserInput();
